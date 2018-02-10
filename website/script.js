7582c43bcd8559dc863695953617080add4396be
zhad3 <dragoonzombie@gmail.com>
2018-02-10 20:54:57 +0100

    Basic slider for tutorial

--- website/script.js
+++ website/script.js
@@ -1,47 +1,95 @@
 var controller = new ScrollMagic.Controller();
 
+// Section 01 Parallax
 new ScrollMagic.Scene({
-    triggerElement: "#problemfinding",
+    triggerElement: "#section01",
     triggerHook: "onEnter",
     duration: "200%"
 })
-.setTween("#problemfinding > div.parallax-box", {y: "80%", ease: Linear.easeNone})
+.setTween("#section01 > div.parallax-box", {y: "80%", ease: Linear.easeNone})
 .addIndicators()
 .addTo(controller);
 
+// Section 02 Parallax
 new ScrollMagic.Scene({
-    triggerElement: "#start",
-    triggerHook: "onLeave",
-    offset: document.getElementById("start").offsetHeight - document.documentElement.clientHeight * 0.5,
-    duration: document.documentElement.clientHeight * 0.5
+    triggerElement: "#section02",
+    triggerHook: "onEnter",
+    duration: "300%"
 })
-.setTween("#start > div > div", {opacity: 0, ease: Linear.easeNone})
+.setTween("#section02 > div.parallax-box", {y: "20%", ease: Linear.easeNone})
 .addIndicators()
 .addTo(controller);
 
+// Section 03 Pin Phone
 new ScrollMagic.Scene({
-    triggerElement: "#start",
+    triggerElement: "#section02",
+    triggerHook: "onCenter",
+    offset: 400,
+    duration: "100%"
+})
+.setPin("#tut-phone")
+.addIndicators()
+.addTo(controller);
+
+// Section 03 Phone 2nd Step
+new ScrollMagic.Scene({
+    triggerElement: "#section02",
+    triggerHook: "onCenter",
+    offset: 600
+})
+.setTween("#tut-step", 0.2, {backgroundPosition: "-323px 0px", ease: Linear.easeNone})
+.addIndicators()
+.addTo(controller);
+
+// Section 03 Phone 3rd Step
+new ScrollMagic.Scene({
+    triggerElement: "#section02",
+    triggerHook: "onCenter",
+    offset: 800
+})
+.setTween("#tut-step", 0.2, {backgroundPosition: "-646px 0px", ease: Linear.easeNone})
+.addIndicators()
+.addTo(controller);
+
+// Section 03 Phone 4th Step
+new ScrollMagic.Scene({
+    triggerElement: "#section02",
+    triggerHook: "onCenter",
+    offset: 1000
+})
+.setTween("#tut-step", 0.2, {backgroundPosition: "-969px 0px", ease: Linear.easeNone})
+.addIndicators()
+.addTo(controller);
+
+// Section 03 Phone 5th Step
+new ScrollMagic.Scene({
+    triggerElement: "#section02",
+    triggerHook: "onCenter",
+    offset: 1200
+})
+.setTween("#tut-step", 0.2, {backgroundPosition: "-1292px 0px", ease: Linear.easeNone})
+.addIndicators()
+.addTo(controller);
+
+
+
+
+// Section 03 Parallax
+new ScrollMagic.Scene({
+    triggerElement: "#section03",
     triggerHook: "onEnter",
     duration: "200%"
 })
-.setTween("#start > div.parallax-box", {y: "80%", ease: Linear.easeNone})
+.setTween("#section03 > div.parallax-box", {y: "80%", ease: Linear.easeNone})
 .addIndicators()
 .addTo(controller);
 
+// Section 04 Parallax
 new ScrollMagic.Scene({
-    triggerElement: "#workshop",
+    triggerElement: "#section04",
     triggerHook: "onEnter",
     duration: "200%"
 })
-.setTween("#workshop > div.parallax-box", {y: "80%", ease: Linear.easeNone})
-.addIndicators()
-.addTo(controller);
-
-new ScrollMagic.Scene({
-    triggerElement: "#prototype",
-    triggerHook: "onEnter",
-    duration: "200%"
-})
-.setTween("#prototype > div.parallax-box", {y: "80%", ease: Linear.easeNone})
+.setTween("#section04 > div.parallax-box", {y: "80%", ease: Linear.easeNone})
 .addIndicators()
 .addTo(controller);
