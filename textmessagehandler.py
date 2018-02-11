4ef274cdcf5ce6f9587e0cff1e0872cb07d6713a
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2018-02-11 14:20:24 +0100

    bugfix + added command handling

--- textmessagehandler.py
+++ textmessagehandler.py
@@ -4,14 +4,20 @@ def handle_msg(msg):
     try:
         text = msg["message"]["text"]
         chat = msg["message"]["chat"]["id"]
-        lectures = db.get_studentlectures(chat)
-        if text in lectures:
-            db.delete_lecture(1, chat)
-            lectures = db.get_studentlectures(chat)
-        else:
-            db.add_lecture(1, chat)
-            lectures = db.get_studentlectures(chat)
-        message = "\n".join(lectures)
-        send_message(message, chat)
+
+        if text.startswith('!'):
+            handle_command(text, chat)
     except KeyError:
         pass
+
+
+def handle_command(text, chat):
+    lectures = db.get_studentlectures(chat)
+    if text[1:] in lectures:
+        db.delete_lecture(1, chat)
+        lectures = db.get_studentlectures(chat)
+    else:
+        db.add_lecture(1, chat)
+        lectures = db.get_studentlectures(chat)
+    message = "\n".join(lectures)
+    send_message(message, chat)
