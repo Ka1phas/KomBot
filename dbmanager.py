376872b00ddd4fcc5bb490125ce31bd3acd19061
Ka1phas <kaiphas@me.com>
2018-02-11 14:53:26 +0100

    New Commands added
    /removelecture + custom keyboard
    /addLecture <LectureNameORId>

--- dbmanager.py
+++ dbmanager.py
@@ -1,28 +1,53 @@
 import sqlite3
 
+
 class DBManager:
+
     def __init__(self, dbname="knowledge.db"):
-        self.dbname = dbname;
+        self.dbname = dbname
         self.connection = sqlite3.connect(dbname)
-        
+
     def setup_timetable(self):
-        stmt = "CREATE TABLE IF NOT EXISTS studentlectures (lecture integer, owner text, FOREIGN KEY (lecture) REFERENCES lecture(lectureid))"
-        self.connection.execute(stmt)
+        tblstmt = ("CREATE TABLE IF NOT EXISTS studentlectures ("
+                   "lecture integer,"
+                   "owner text,"
+                   "FOREIGN KEY (lecture) REFERENCES lecture(lectureid))")
+        ownidx = ("CREATE INDEX IF NOT EXISTS ownIndex"
+                  " ON studentlectures (owner ASC)")
+        self.connection.execute(tblstmt)
+        self.connection.execute(ownidx)
         self.connection.commit()
-        
+
     def add_lecture(self, lecture_id, owner):
-        stmt = "INSERT INTO studentlectures (lecture, owner) VALUES (?,?)"
+        stmt = ("INSERT INTO studentlectures (lecture, owner)"
+                " VALUES (?,?)")
         args = (lecture_id, owner)
         self.connection.execute(stmt, args)
         self.connection.commit()
-        
+
     def delete_lecture(self, lecture_id, owner):
-        stmt = "DELETE FROM studenlectures WHERE lecture = (?) AND owner = (?)"
+        stmt = ("DELETE FROM studentlectures"
+                " WHERE lecture = (?)"
+                " AND owner = (?)")
         args = (lecture_id, owner)
         self.connection.execute(stmt, args)
         self.connection.commit()
-        
+
+    def get_lecture_id(self, lecture_title):
+        stmt = ("SELECT lectureid FROM lecture"
+                " WHERE title = (?)")
+        args = (lecture_title, )
+        result = self.connection.execute(stmt, args).fetchone()
+        print(result)
+        if result:
+            return int(result[0])
+        else:
+            return -1
+
     def get_studentlectures(self, owner):
-        stmt = "SELECT title FROM lecture INNER JOIN studentlectures ON studentlectures.lecture = lecture.lectureid WHERE studentlectures.owner = (?)"
+        stmt = ("SELECT title FROM lecture"
+                " INNER JOIN studentlectures"
+                " ON studentlectures.lecture = lecture.lectureid"
+                " WHERE studentlectures.owner = (?)")
         args = (owner, )
         return [x[0] for x in self.connection.execute(stmt, args)]
