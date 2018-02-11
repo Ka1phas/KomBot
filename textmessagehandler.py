a44893e26b45a85f49f8ed4779f80c8746b06b7d
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2018-02-11 14:39:24 +0100

    started with pattern matching implementation

--- textmessagehandler.py
+++ textmessagehandler.py
@@ -1,4 +1,8 @@
 from kombot import send_message, db
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def handle_msg(msg):
     try:
@@ -7,8 +11,24 @@ def handle_msg(msg):
 
         if text.startswith('!'):
             handle_command(text, chat)
+        else:
+            get_pattern_match(text, chat)
     except KeyError:
         pass
+
+
+def get_pattern_match(text, chat):
+    with open(os.path.join(__location__, 'patterns.dat')) as file:
+        for line in file:
+            skill_name =  line.split(' ', 1)[0]
+            skill_text = line.split(' ', 1)[1]
+            skill_patterns = skill_text.split("...")
+            print(skill_patterns[0])
+            print(text)
+            if text.startswith(skill_patterns[0]):
+                send_message(skill_name, chat)
+                break
+            print(line)
 
 
 def handle_command(text, chat):
