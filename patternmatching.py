dc0f96a2fa52c25a3eb39b78ce74f9692f388b4b
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2018-02-12 16:17:37 +0100

    added patterns

--- patternmatching.py
+++ patternmatching.py
@@ -12,7 +12,9 @@ def get_skill_match(skill_name, skill_text, text, chat):
     g_text = text
     g_chat = chat
 
-    if skill_name == "GetExamRegistration":
+    if skill_name == "GetCertificateOfStudy":
+        return get_certificate_of_study()
+    elif skill_name == "GetExamRegistration":
         return get_exam_registration()
     elif skill_name == "GetFreeSoftware":
         return get_free_software()
@@ -20,18 +22,32 @@ def get_skill_match(skill_name, skill_text, text, chat):
         return get_how_to_use()
     elif skill_name == "GetLectureTime":
         return get_lecture_time()
-    elif skill_name == "GetSemesterFee":
-        return get_semester_fee()
     elif skill_name == "GetRoom":
         return get_semester_fee()
+    elif skill_name == "GetSemesterFee":
+        return get_semester_fee()
+    elif skill_name == "GetVPN":
+        return get_vpn()
     else:
         print("Error: No pattern " + skill_name + " found.")
 
     return False
 
+def get_certificate_of_study():
+    global g_chat
+    answer = "Die Studienbescheinigung ist unter folgendem Link verfügbar:\n" \
+             "https://campus.uni-due.de/cm/ \n \n" \
+             "Schritte:\n" \
+             "1.) Mit Unikennung einloggen \n" \
+             "2.) Auf \"Mein Studium\" -> \"Studienservice\" -> \"Bescheinigungen\" klicken \n" \
+             "3.) .pdf-Datei mit Studienbescheinigung runterladen "
+    send_message(answer, g_chat)
+    return True
+
+
 def get_exam_registration():
     global g_chat
-    answer = "Die Prüfungsanmeldephase für das Sommersemester 2018 beginnt am 07.05.2018 und endet am 18.05.18.\n" \
+    answer = "Die Prüfungsanmeldephase für das Sommersemester 2018 beginnt am 07.05.18 und endet am 18.05.18.\n" \
              "Eine Prüfung kann über folgendes Portal angemeldet werden: \n\n" \
              "https://campus.uni-due.de/lsf  \n\n" \
              "Terminpläne für Bachelor und Master sind unter folgendem Link zu finden: \n\n" \
@@ -61,5 +77,15 @@ def get_semester_fee():
     global g_chat
     answer = "Der Sozial- und Studierendenschaftsbeitrag beträgt für das Sommersemester 2018 insgesamt 304,62 €.\n" \
              "Der Beitrag kann bis zum 02.03.2018 überwiesen werden."
+    send_message(answer, g_chat)
+    return True
+
+def get_vpn():
+    global g_chat
+    answer = "Der Zugriff auf Uni-Dienste von z.B. Zuhause aus kann über eine VPN-Verbindung gewährleistet werden." \
+             "Windows-Nutzer können hierfür Cisco AnyConnect verwenden: \n" \
+             "https://www.uni-due.de/zim/services/internetzugang/cisco-vpn-anyconnect \n\n" \
+             "Für MacOS und Linux bietet sich OpenConnect an: \n" \
+             "https://www.uni-due.de/zim/services/internetzugang/openconnect.php"
     send_message(answer, g_chat)
     return True
